a04cc65761a72e3dde47aac4d749e7b2f5abe89f
Abner David Dotel Chalas <dotelchalas@gmail.com>
2024-12-02 07:31:27 -0400

    Paquetes agregdos con exito

---
+++ ClinicaTest/SeleniumTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.Extensions;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace ClinicaTest
+{
+    internal class SeleniumTest
+    {
+    }
+}
